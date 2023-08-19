822b4ea544c6d3ce8b878a26ca5646d2b2fafdbe
zinebadi <zineb.eloubbadi@gmail.com>
2023-08-19 20:49:23 +0100

    helper methos

---
+++ lib/game_board.dart
@@ -0,0 +1,29 @@
+import 'package:chess_app/components/square.dart';
+import 'package:chess_app/helper/helper_methods.dart';
+import 'package:flutter/material.dart';
+
+class GameBoard extends StatefulWidget {
+  const GameBoard({super.key});
+
+  @override
+  State<GameBoard> createState() => _GameBoardState();
+}
+
+class _GameBoardState extends State<GameBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+        itemCount: 8 * 8,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
+        itemBuilder: (context, index) {
+          return Square(
+            isWhite: isWhite(index),
+          );
+        },
+      ),
+    );
+  }
+}
